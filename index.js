cccedb6084e0527d6e72c7ecdc9e89fae842e859
Simon Morgan <sjm@sjm.io>
2017-06-23 15:34:58 +0100

    Simplify code

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ require("dotenv").config();
 
 const Botkit = require("botkit");
 
-if (!process.env.BOT_OAUTH_TOKEN || !process.env.PORT) {
+if (!process.env.BOT_OAUTH_TOKEN) {
   console.error(
-    "Please specify BOT_OAUTH_TOKEN and PORT in environment variables");
+    "BOT_OAUTH_TOKEN environment variable not defined");
   process.exit(1);
 }
 
@@ -14,13 +14,7 @@ const controller = Botkit.slackbot({});
 
 const bot = controller.spawn({
   token: process.env.BOT_OAUTH_TOKEN
-});
-
-bot.startRTM(function (err) {
-  if (err) {
-    console.error(err);
-  }
-});
+}).startRTM();
 
 process.on("exit", bot.destroy);
 process.on("SIGINT", () => {
